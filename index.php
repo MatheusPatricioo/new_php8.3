6784e60eae7667ce5af12f1276899ca023396355
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-10-04 12:00:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
 <?php
-// $cidades = ["rio","sp","mg","bh","to"];
-// echo "a terceira cidade é: " . $cidades[2];
+// $usuarioCorreto = 'admin';
+// $senhaCorreta = '123456';
+// $usuario = 'admind123';
+// $senha = '123456';
 
-$alunos = ["matheus"=>28,"lucas"=>21,"paulo"=>38];
- echo $alunos["paulo"];
 
-// $cores = ["verde", "azul", "lilas"];
+//     if ($usuario == $usuarioCorreto && $senha == $senhaCorreta) {
+//         echo "seja bem vindo!";
+//     } else if ($usuario != $usuarioCorreto) {
+//         echo "usuario incorreto";
+    
+//     } else if ($senha !=$senhaCorreta){ 
+//         echo "senha incorreta";
+//     } else {
+//         echo "esta tudo errado";
+//     }
 
-// if ($cores = [2]) {
-//     $index =2 == "rosa";
-// }
 
-// echo $cores=[];
+//  $idade = 8;
+//  if($idade>=18) {
+//     echo"pode entrar";
+//  } else {
+//     echo "nao pode entrar";
+//  };
+
+// $valorCompra= 500;
+// $desconto;
+// $valorfinal;
+// if($valorCompra>=200){
+//     $desconto=$valorCompra*0.2;
+//     $valorfinal=$valorCompra - $desconto;
+//     echo"o valor total ficou em: ".$valorfinal."\n";
+// } else if($valorCompra< 200){
+//     echo"Sem descontos: ".$valorCompra."\n";
+// };
 ?> 
