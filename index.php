6cb8140b4e097f49c4802d864ce35cd0a6ea87db
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-10-04 11:18:29 -0300

    up

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// $cidades = ["rio","sp","mg","bh","to"];
+// echo "a terceira cidade é: " . $cidades[2];
+
+$alunos = ["matheus"=>28,"lucas"=>21,"paulo"=>38];
+ echo $alunos["paulo"];
+
+// $cores = ["verde", "azul", "lilas"];
+
+// if ($cores = [2]) {
+//     $index =2 == "rosa";
+// }
+
+// echo $cores=[];
+?> 
